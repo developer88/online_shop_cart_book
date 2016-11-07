9bdd074cff40920fd51e42be14dcdb327d93e4fb
Andrey Eremin <andrey.eremin@xing.com>
2016-11-07 15:56:16 +0100

    Work on file structure for Store class

--- lib/super_online_cart.rb
+++ lib/super_online_cart.rb
@@ -3,3 +3,6 @@ require "super_online_cart/version"
 module SuperOnlineCart
   # Your code goes here...
 end
+
+require "super_online_cart/store"
+require "super_online_cart/currency"
---
+++ lib/super_online_cart/store.rb
@@ -0,0 +1,4 @@
+class SuperOnlineCart::Store
+
+
+end
