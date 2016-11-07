6851d65c622c549e196c70f5c656180b1db8e6f2
Andrey Eremin <andrey.eremin@xing.com>
2016-11-07 16:45:10 +0100

    Work on Store class

--- lib/super_online_cart/store.rb
+++ lib/super_online_cart/store.rb
@@ -1,4 +1,28 @@
 class SuperOnlineCart::Store
+  def initialize(session:)
+    @session = session
+    initialize_session
+  end
 
+  def goods
+    @session[:goods]
+  end
 
+  def get(good_id:)
+    goods[good_id.to_s].to_i
+  end
+
+  def set(good_id:, good_amount:)
+    goods[good_id.to_s] = good_amount
+  end
+
+  def remove(good_id:)
+    goods.delete(good_id.to_s)
+  end
+
+  private
+
+  def initialize_session
+    @session[:goods] ||= {}
+  end
 end
--- spec/super_onlince_cart/store_spec.rb
+++ spec/super_onlince_cart/store_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe SuperOnlineCart::Store do
+  describe 'initialization' do
+    it 'initializes all necessary objects' do
+      expect_any_instance_of(described_class).to receive(:initialize_session).and_call_original
+      expect(described_class.new(session: {}).goods).to eq({})
+    end
+  end
 
+  describe '#goods' do
+    let(:session) { {goods: {"1" => 1}} }
+    it 'returns goods from session' do
+      expect(described_class.new(session: session).goods).to eq(session[:goods])
+    end
+  end
+
+  describe '#get' do
+
+  end
+
+  describe '#set' do
+
+  end
+
+  describe '#remove' do
+
+  end
 end
